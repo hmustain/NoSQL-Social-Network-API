66e17e325876d73009fa9a301cbcb465bbb92461
hmustain <hunter@bunkerbranding.com>
2023-02-22 17:03:24 -0600

    issue 30 delete reaction

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -66,5 +66,20 @@ addReaction({ params, body }, res) {
       })
       .catch((err) => res.status(400).json(err));
   },
-  
+
+// Delete a reaction from a thought
+deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true, runValidators: true }
+    )
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        return res.json(thought);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
