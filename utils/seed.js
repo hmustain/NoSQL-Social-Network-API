e1750a714a5c6842a32359b9cae9fc94e4e0e460
hmustain <hunter@bunkerbranding.com>
2023-02-22 11:16:08 -0600

    issue 10 create a seed.js file inside utils folder

---
+++ utils/seed.js
@@ -0,0 +1,28 @@
+const connection = require('../config/connection');
+const { users, thoughts } = require('./data');
+const { User, Thought } = require('../models');
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', async () => {
+  console.log('connected to database');
+
+  try {
+    // Delete existing users and thoughts
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    // Create new users and thoughts
+    const createdUsers = await User.insertMany(users);
+    const thoughtsWithUsers = thoughts.map(thought => {
+      const user = createdUsers.find(user => user.username === thought.username);
+      return { ...thought, username: user._id };
+    });
+    await Thought.insertMany(thoughtsWithUsers);
+
+    console.log('Database seeded successfully!');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    process.exit(1);
+  }
+});
