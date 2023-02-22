e5cf44cb22347b8f1e4dedce8cb649c11fe5e0c0
hmustain <hunter@bunkerbranding.com>
2023-02-22 16:48:04 -0600

    issue 24 & 25 get thoughts and thoughts by id

---
+++ controller/thoughtController.js
@@ -0,0 +1,22 @@
+const { Thought } = require('../models');
+
+module.exports = {
+    // GET all thoughts
+    getAllThoughts(req, res) {
+        Thought.find()
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+
+// Get thought by id
+getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+    .then((thought) => {
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought with this id!'})
+        }
+        return res.json(thought);
+    })
+    .catch((err) => res.status(400).json(err));
+}
+};
