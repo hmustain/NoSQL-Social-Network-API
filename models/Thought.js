e27f4d2f87c266ecc12bbb00cb40c287353288a0
hmustain <hunter@bunkerbranding.com>
2023-02-22 10:40:19 -0600

    issue 5 setup thought model w/ issue 7 setup reaction schema only

---
+++ models/Thought.js
@@ -0,0 +1,79 @@
+const { time } = require('console');
+const { Schema, model } = require('mongoose')
+
+// Create a reaction schema
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+// Create Thought schema model
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            minLength: 1,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
+// function to format date
+function dateFormat(timestamp) {
+    return new Date(timestamp).toLocaleDateString();
+}
