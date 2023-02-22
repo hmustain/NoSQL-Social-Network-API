1be2c749687d7dcee85a03f7b4d4b81f7f4a4ebe
hmustain <hunter@bunkerbranding.com>
2023-02-22 15:37:15 -0600

    issues 17-22 user routes tested and working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,16 +12,16 @@ const {
 
 
 //  /api/users
-router.route('/').get(getAllUsers);
+router.route('/').get(getAllUsers).post(createUser);
 
-// .post(createUser);
+
 
 
 // /api/users/:id
-// router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 
 //  /api/users/:userId/friends/:friendsId
-// router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
